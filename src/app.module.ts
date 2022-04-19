dc6e1381ce04355b2081c4335d93c61a77547c5b
all-you-need-is-laugh <smerost2@gmail.com>
2022-04-19 17:05:47 +0200

    Removed: redundant files from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [ CoffeesModule ],
-  controllers: [ AppController ],
-  providers: [ AppService ],
+  controllers: [ ],
+  providers: [ ],
 })
 export class AppModule {}
