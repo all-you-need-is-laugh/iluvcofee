325b0e03ef3b4a40b2be5e53ad6f23692f7bb50c
all-you-need-is-laugh <smerost2@gmail.com>
2022-05-31 18:25:28 +0200

    Fixed: ESLint warnings and errors

--- test/utils/type-convertions.ts
+++ test/utils/type-convertions.ts
@@ -1,12 +1,12 @@
 import { assertArray, assertObject } from './assertions';
 
-export function asArray(arg: unknown): unknown[] {
+export function asArray (arg: unknown): unknown[] {
   assertArray(arg);
 
   return arg;
 }
 
-export function asObject(arg: unknown): Record<string, unknown> {
+export function asObject (arg: unknown): Record<string, unknown> {
   assertObject(arg);
 
   return arg;
